c440b452ce1f699ed378c677f6749fa9348fdccf
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-01 19:42:04 +0530

    [Nithin] Add : updation of PincodeValidation

---
+++ Pincode/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pincode
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Sample Demo of REGEX");
+            ValidatePincode obj = new ValidatePincode();
+            obj.Validate("516001");
+        }
+    }
+}
---
+++ Pincode/ValidatePincode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pincode
+{
+    public class ValidatePincode
+    {
+        string uc1 = "^[0-9]{6}$";
+        public void Validate(string input )
+        {
+            bool result = Regex.IsMatch(input, uc1);
+                if(result)
+                    Console.WriteLine("Valid");
+                else
+                    Console.WriteLine("! inValid");
+        }
+
+
+    }
+}
