9c0c578e17c37b8ce6f9e1c09c0e59c8895df33e
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-02 15:15:25 +0530

    [Nithin] Add : Updation of Email validation

--- Pincode/Program.cs
+++ Pincode/Program.cs
@@ -6,23 +6,64 @@ namespace Pincode
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Sample uc1_REGEX !");
-            ValidatePincode uc1 = new ValidatePincode();
-            uc1.uc1Validate("516001");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the Option :\n1.Validate Pincode \n2.Validate Email\n3.Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
 
-            Console.WriteLine("Sample uc2_REGEX !");
-            ValidatePincode uc2 = new ValidatePincode();
-            uc2.uc2Validate("516001");
+                {
+                    case 1:
+                        Console.WriteLine("Sample uc1_Pincode !");
+                        ValidatePincode uc1 = new ValidatePincode();
+                        uc1.uc1Validate("516001");
 
-            Console.WriteLine("Sample uc3_REGEX !");
-            ValidatePincode uc3 = new ValidatePincode();
-            uc3.uc3Validate("516001");
+                        Console.WriteLine("Sample uc2_Pincode !");
+                        ValidatePincode uc2 = new ValidatePincode();
+                        uc2.uc2Validate("516001");
 
-            Console.WriteLine("Sample uc4_REGEX !");
-            ValidatePincode uc4 = new ValidatePincode();
-            uc4.uc4Validate("516 001");
+                        Console.WriteLine("Sample uc3_Pincode !");
+                        ValidatePincode uc3 = new ValidatePincode();
+                        uc3.uc3Validate("516001");
 
-           
+                        Console.WriteLine("Sample uc4_Pincode !");
+                        ValidatePincode uc4 = new ValidatePincode();
+                        uc4.uc4Validate("516 001");
+                        break;
+                    case 2:
+                        Console.WriteLine("Sample UC1_email !");
+                        ValidateEmail uc1Email = new ValidateEmail();
+                        uc1Email.uc1Validate("abc");
+
+                        Console.WriteLine("Sample UC2_email !");
+                        ValidateEmail uc2Email = new ValidateEmail();
+                        uc2Email.uc2Validate("@bridelabz");
+
+                        Console.WriteLine("Sample UC3_email !");
+                        ValidateEmail uc3Email = new ValidateEmail();
+                        uc3Email.uc3Validate(".co");
+
+                        Console.WriteLine("Sample UC4_email !");
+                        ValidateEmail uc4Email = new ValidateEmail();
+                        uc4Email.uc4Validate("abc.xyz@bridgelabz.com");
+
+                        Console.WriteLine("Sample UC5_Full Email : !");
+                        ValidateEmail uc5Email = new ValidateEmail();
+                        uc5Email.uc5Validate("abc.xyz@bridgelabz.com");
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("!! Enter the Correct input !!");
+                        break;
+
+                }
+
+
+            }
+
         }
     }
 }
---
+++ Pincode/ValidateEmail.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Pincode
+{
+    public class ValidateEmail
+    {   //abc.xyz@bridgelabz.co.in
+        string uc1Regex = "^[a-z]{0,}$";
+        string uc2Regex = "^[@]{1}[a-z]{1,}$";
+        string uc3Regex = "^[.]{1}[a-z]{2,3}$";
+        string uc4Regex = "^[a-z]+[._+-]{0,1}[a-z]{0,}[@]{1}[a-z]{1,}[.]{1}[a-z]{2,}$";
+        string actualRegex = "^[a-z]{1,}[._+-]{0,1}[a-z]{1,}[@]{1}[a-z]{1,}[.]{1}[a-z]{2,}[.]{0,1}[a-z]{0,2}$";
+
+        public void uc1Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1Regex);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc2Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc2Regex);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc3Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc3Regex);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc4Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc4Regex);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc5Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, actualRegex);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+
+    }
+}
