abc646d892b1ff58d24991f15b8c8c7a50372ebf
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-08-02 12:55:17 +0530

    [Nithin] Add : Updation of Validating the Pincode

--- Pincode/Program.cs
+++ Pincode/Program.cs
@@ -4,11 +4,26 @@ namespace Pincode
 {
     public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Sample Demo of REGEX !");
-            ValidatePincode obj = new ValidatePincode();
-            obj.Validate("516001");
+            Console.WriteLine("Sample uc1_REGEX !");
+            ValidatePincode uc1 = new ValidatePincode();
+            uc1.uc1Validate("516001");
+
+            Console.WriteLine("Sample uc2_REGEX !");
+            ValidatePincode uc2 = new ValidatePincode();
+            uc2.uc2Validate("516001");
+
+            Console.WriteLine("Sample uc3_REGEX !");
+            ValidatePincode uc3 = new ValidatePincode();
+            uc3.uc3Validate("516001");
+
+            Console.WriteLine("Sample uc4_REGEX !");
+            ValidatePincode uc4 = new ValidatePincode();
+            uc4.uc4Validate("516 001");
+
+            //ValidatePincode uc1 = new ValidatePincode();
+            //uc1.uc1Validate("abc");
         }
     }
 }
--- Pincode/ValidatePincode.cs
+++ Pincode/ValidatePincode.cs
@@ -11,13 +11,38 @@ namespace Pincode
     public class ValidatePincode
     {
         string uc1 = "^[0-9]{6}$";
-        public void Validate(string input )
+        string uc4 = "^[0-9]{3}[ ][0-9]{3}$";
+        public void uc1Validate(string input )
         {
             bool result = Regex.IsMatch(input, uc1);
                 if(result)
                     Console.WriteLine("Valid");
                 else
                     Console.WriteLine("! inValid");
+        }
+        public void uc2Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc3Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc1);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
+        }
+        public void uc4Validate(string input)
+        {
+            bool result = Regex.IsMatch(input, uc4);
+            if (result)
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine("! inValid");
         }
 
 
